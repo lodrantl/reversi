0d64d2031103b25491549f2e0d27ab15225d9b01
lenarttreven <lenart.treven@student.fmf.uni-lj.si>
2017-03-28 16:50:49 +0200

    Merge branch 'master' of /Users/lenarttreven/Desktop/reversi with conflicts.

--- reversi/igra.py
+++ reversi/igra.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class Stanje():
     BELO = 'belo'
     CRNO = 'crno'
@@ -6,11 +8,10 @@ class Stanje():
 
 class Igra:
     deska = [[Stanje.PRAZNO for _ in range(8)] for _ in range(8)]
-    na_potezi = Stanje.BELO
+    poteza = Stanje.BELO
     mozne_poteze = set()
-    stevilo_belih = 2
-    stevilo_crnih = 2
-
+    stevilo_belih = 0
+    stevilo_crnih = 0
     def __init__(self):
         self.deska[3][3] , self.deska[4][4] = Stanje.BELO, Stanje.BELO
         self.deska[3][4], self.deska[4][3] = Stanje.CRNO, Stanje.CRNO
@@ -33,9 +34,9 @@ class Igra:
                 while 0 <= i <= 7 and 0<= j <= 7:
                     if self.deska[i][j] == Stanje.PRAZNO:
                         break
-                    if self.deska[i][j] == self.na_potezi and not zastavica:
+                    if self.deska[i][j] == self.poteza and not zastavica:
                         break
-                    if self.deska[i][j] == self.na_potezi and zastavica:
+                    if self.deska[i][j] == self.poteza and zastavica:
                         return True
                     i  += smer_x
                     j += smer_y
@@ -46,6 +47,16 @@ class Igra:
     def odigraj_potezo(self, koordinate):
         x, y = koordinate
         if (x, y) in self.mozne_poteze:
+            self.deska[x][y] = self.poteza
+            print(self.deska[x][y])
+            polja_za_obrnit = self.preveri_katera_polja_za_obrnit(x, y)
+            print(polja_za_obrnit)
+            for polje in polja_za_obrnit:
+                self.obrni_zeton(polje)
+            if self.poteza == Stanje.BELO:
+                self.poteza = Stanje.CRNO
+            elif self.poteza == Stanje.CRNO:
+                self.poteza = Stanje.BELO
             self.deska[x][y] = self.na_potezi
             if self.na_potezi == Stanje.BELO:
                 self.stevilo_belih += 1
@@ -54,11 +65,42 @@ class Igra:
                 self.stevilo_crnih += 1
                 self.na_potezi = Stanje.BELO
             self.mozne_poteze = self.dobi_mozne_poteze()
+            print(self.mozne_poteze)
+
         else:
-            print("Polje ni na izbiro: ", koordinate)
+            print("Polje ni na izbiro")
             pass
 
-    def obrni_zeton(self, x, y):
+    def nasprotno_stanje(self, stanje):
+        if stanje == Stanje.BELO:
+            return Stanje.CRNO
+        elif stanje == Stanje.CRNO:
+            return Stanje.BELO
+
+    def preveri_katera_polja_za_obrnit(self, x, y):
+        polja_za_obrnit = []
+        for smer_x, smer_y in [(1, 0), (0, 1), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
+            i, j = x + smer_x,  y + smer_y
+            trenutna_polja = []
+            while 0 <= i and i <= 7 and 0 <= j and j <= 7:
+                print((i, j))
+                if self.deska[i][j] == self.poteza:
+                    polja_za_obrnit += trenutna_polja
+                    break
+                elif self.deska[i][j] == self.nasprotno_stanje(self.poteza):
+                    trenutna_polja.append((i, j))
+                    i += smer_x
+                    j += smer_y
+                elif self.deska[i][j] == Stanje.PRAZNO:
+                    break
+        return polja_za_obrnit
+
+    def vstavi_mozne_poteze(self):
+        for (x, y) in self.mozne_poteze:
+            self.deska[x][y] = Stanje.MOGOCE
+
+    def obrni_zeton(self, koordinate):
+        x, y = koordinate
         if self.deska[x][y] == Stanje.BELO:
             self.stevilo_belih -= 1
             self.stevilo_crnih += 1
