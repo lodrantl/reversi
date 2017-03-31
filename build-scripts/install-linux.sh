4000761aaab67788b3b25caf76a018f4537ca1ca
Luka Lodrant <luka.null@gmail.com>
2017-04-01 01:59:12 +0200

    Travis mac spec fix

--- build-scripts/install-linux.sh
+++ build-scripts/install-linux.sh
@@ -6,7 +6,7 @@ sudo lsb_release -a
 #Install kivy dependencies
 sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo apt-get update
-sudo apt-get install -y  build-essential  git ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
+sudo apt-get install -y  build-essential upx-ucl git ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
 pip3 install --upgrade pip virtualenv setuptools
 pip3 install Cython
 pip3 install pygments docutils pyinstaller
--- build-scripts/install-mac.sh
+++ build-scripts/install-mac.sh
@@ -2,12 +2,12 @@
 
 #Install and check python
 brew install python3
-
 #Install kivy dependencies
 brew install --build-bottle sdl2 sdl2_image sdl2_ttf sdl2_mixer
 pip3 install -I Cython==0.23
 
-#Install pyinstaller
+#Install pyinstaller and upx
+brew install upx
 pip3 install pyinstaller
 
 #Install kivy master branch
