7a4c386d00b1e7791c99c8e09073d74b8f7e3144
Luka Lodrant <luka.null@gmail.com>
2017-04-01 01:30:55 +0200

    Travis mac python setup

--- build-scripts/install-linux.sh
+++ build-scripts/install-linux.sh
@@ -1,8 +1,15 @@
+#!/usr/bin/env bash
+
+#Print Ubuntu version info
 sudo lsb_release -a
+
+#Install kivy dependencies
 sudo add-apt-repository -y ppa:mc3man/trusty-media
 sudo apt-get update
 sudo apt-get install -y  build-essential  git ffmpeg libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
-pip install --upgrade pip virtualenv setuptools
-pip install Cython
-pip install pygments docutils pyinstaller
-pip install git+https://github.com/kivy/kivy.git@master
+pip3 install --upgrade pip virtualenv setuptools
+pip3 install Cython
+pip3 install pygments docutils pyinstaller
+
+#Install kivy master branch
+pip3 install git+https://github.com/kivy/kivy.git@master
--- build-scripts/install-mac.sh
+++ build-scripts/install-mac.sh
@@ -1,8 +1,14 @@
-python -version
-python3 -version
-brew install python3
-python3 -version
+#!/usr/bin/env bash
+
+#Install and check python
+brew install python3@3.5.2
+
+#Install kivy dependencies
 brew install sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
-pip install -I Cython==0.23
-pip install pyinstaller
-USE_OSX_FRAMEWORKS=0 pip install https://github.com/kivy/kivy/archive/master.zip
+pip3 install -I Cython==0.23
+
+#Install pyinstaller
+pip3 install pyinstaller
+
+#Install kivy master branch
+USE_OSX_FRAMEWORKS=0 pip3 install https://github.com/kivy/kivy/archive/master.zip
