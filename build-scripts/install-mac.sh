85e61291bc79fbfb6ff9a251d8923fbfb37789ae
Luka Lodrant <luka.null@gmail.com>
2017-04-01 18:05:46 +0200

    Travis OSX python

--- build-scripts/install-mac.sh
+++ build-scripts/install-mac.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 set -x #echo on
 
-#Install and check python
-brew update
+export PYENV_VERSION=$PYTHON
+
 # Per the `pyenv homebrew recommendations <https://github.com/yyuu/pyenv/wiki#suggested-build-environment>`_.
 brew install openssl readline
+
 # See https://docs.travis-ci.com/user/osx-ci-environment/#A-note-on-upgrading-packages.
 # I didn't do this above because it works and I'm lazy.
 brew outdated pyenv || brew upgrade pyenv
 
 pyenv install $PYENV_VERSION
-
-# I would expect something like ``pyenv init; pyenv local $PYTHON`` or
-# ``pyenv shell $PYTHON`` would work, but ``pyenv init`` doesn't seem to
-# modify the Bash environment. ??? So, I hand-set the variables instead.
-export PYENV_VERSION=$PYTHON
 export PATH="/Users/travis/.pyenv/shims:${PATH}"
 
 # A manual check that the correct version of Python is running.
